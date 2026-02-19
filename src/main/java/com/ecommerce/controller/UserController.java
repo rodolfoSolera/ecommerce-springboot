31503e70dce9c8ad8cd20512a47d10cb4e98bbe1
Rodolfo P. Solera <rodolfosolera.dev@gmail.com>
2026-02-18 21:46:51 -0300

    Auth: Aplicado Filtro na busca de usuarios

--- src/main/java/com/ecommerce/controller/UserController.java
+++ src/main/java/com/ecommerce/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +10,7 @@ import com.ecommerce.entity.User;
 import com.ecommerce.repository.UserRepository;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -17,15 +19,21 @@ public class UserController {
     public UserRepository userRepository;
 
     @GetMapping("/user")
-    public String user(Model model) {
+    public String user(Model model, @RequestParam(required = false) User.UserStatus status) {
         model.addAttribute("adminSection","user");
 
         model.addAttribute("user", new User());
 
         Iterable<User> users;
 
-        users = userRepository.findAll();
+        if (status != null) {
+            users = userRepository.findByStatus(status);
+        } else {
+            users = userRepository.findAll();
+        }
 
+        model.addAttribute("statusFilter", status != null ? status : "ALL");
+        model.addAttribute("status", User.UserStatus.values());
         model.addAttribute("users", users);
         return "admin";
     }
